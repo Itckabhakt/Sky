b60e947c242b8f17af333871ed0294544fbae5b5
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-10-27 00:12:38 +0530

    Update channelData3.js

--- channelData3.js
+++ channelData3.js
@@ -8,4 +8,22 @@ const channelData = {
             }
         }
     },
+    "P1": {
+        "file": "https://finallystream.podik62652.workers.dev/https://linear001-ie-dash1-prd-ak.cdn.skycdp.com/016a/Content/DASH_003_720_120/Live/channel(premiersports1)/manifest_720.asa.mpd", 
+        "drm": {
+            "clearkey": {
+                "keyId": "0005b3dba78ae0fe7a9288022d3b3736",
+                "key": "d2827df80dd7e9243ba6b5bbda5dfa82" 
+            }
+        }
+    },
+    "L1": {
+        "file": "https://ottb.live.cf.ww.aiv-cdn.net/dub-nitro/live/clients/dash/enc/fpndxmzw6o/out/v1/70a50b1bda944628b8e7e66ab4069419/cenc.mpd", 
+        "drm": {
+            "clearkey": {
+                "keyId": "fc12a94c41e0885dbea8a8d94f0a277d",
+                "key": "fc045df077e7669666b1b230e9aa3901" 
+            }
+        }
+    },
   };
