142bf150da9be7c02bd201b6e7ccdb4749e11f22
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-11-03 08:17:13 +0530

    Update channelData3.js

--- channelData3.js
+++ channelData3.js
@@ -1,6 +1,6 @@
 const channelData = {
     "Skycricket": {
-        "file": "https://floral-mode-c21b.podik62652.workers.dev/linear007-gb-dash1-prd-ak.cdn.skycdp.com/016a/Content/DASH_003_720_120/Live/channel(skysportscricket)/manifest_720-120.mpd", 
+        "file": "https://finallystream.podik62652.workers.dev/https://linear007-gb-dash1-prd-ak.cdn.skycdp.com/016a/Content/DASH_003_720_120/Live/channel(skysportscricket)/manifest_720-120.asa.mpd", 
         "drm": {
             "clearkey": {
                 "keyId": "00053ac1e9b1231eed3eb4e3540b3ae1",
