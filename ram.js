38e38d6465dbe07478ae0edec8d5a28bf50646ee
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-11-28 11:14:00 +0530

    Update ram.js

--- ram.js
+++ ram.js
@@ -1,14 +1,3 @@
-// Authorization token
+
+// auth.js
 const AUTH_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ3bXZlciI6Miwid21pZGZtdCI6ImFzY2lpIiwid21pZHR5cCI6MSwid21rZXl2ZXIiOjEsIndtaWRsZW4iOjUxMiwid21pZCI6IlpHRm1aV00yTmpndFptVTJOUzAwWkRkaUxXRmxPVGN0TXpOaE5EWTRaVE5qT0RBeiIsIndtb3BpZCI6MzIsImV4cCI6MTczMjk1MzE3NSwiaWF0IjoxNzMyMzQ5MTc1fQ.qFZPZuFarIMaYHYkTQ44NaiiMomFTaJ8-yHTGz5FLH4';
-
-// Allowed domain
-const allowedDomain = "finallystream.pages.dev";
-
-// Get the current domain
-const currentDomain = window.location.hostname.toLowerCase();
-
-// Check if the current domain matches the allowed domain
-if (currentDomain !== allowedDomain.toLowerCase()) {
-  // Redirect to the specified URL if the domain is not allowed
-  window.location.href = "https://telegram.me/FinallyLiveOwner";
-}
