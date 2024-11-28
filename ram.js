5893ed3b863a44c0a00a8c7ff2f312eb9cd0eed3
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-11-28 11:07:16 +0530

    Update ram.js

--- ram.js
+++ ram.js
@@ -1,12 +1,14 @@
-// auth.js
+// Authorization token
 const AUTH_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ3bXZlciI6Miwid21pZGZtdCI6ImFzY2lpIiwid21pZHR5cCI6MSwid21rZXl2ZXIiOjEsIndtaWRsZW4iOjUxMiwid21pZCI6IlpHRm1aV00yTmpndFptVTJOUzAwWkRkaUxXRmxPVGN0TXpOaE5EWTRaVE5qT0RBeiIsIndtb3BpZCI6MzIsImV4cCI6MTczMjk1MzE3NSwiaWF0IjoxNzMyMzQ5MTc1fQ.qFZPZuFarIMaYHYkTQ44NaiiMomFTaJ8-yHTGz5FLH4';
+
 // Allowed domain
-var allowedDomain = "finallystream.pages.dev";
+const allowedDomain = "finallystream.pages.dev";
 
-// Check current domain
-var currentDomain = window.location.hostname;
+// Get the current domain
+const currentDomain = window.location.hostname.toLowerCase();
 
-// Redirect if the current domain doesn't match the allowed domain
-if (currentDomain !== allowedDomain) {
-  window.location.href = "https://telegram.me/FinallyLiveOwner"; // Redirect to your preferred URL
+// Check if the current domain matches the allowed domain
+if (currentDomain !== allowedDomain.toLowerCase()) {
+  // Redirect to the specified URL if the domain is not allowed
+  window.location.href = "https://telegram.me/FinallyLiveOwner";
 }
